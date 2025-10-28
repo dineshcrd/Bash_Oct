0dec53bde785000dd3e96121e999845fb1d80776
dineshcrd <cloudtechnology129@gmail.com>
2025-10-28 21:02:36 +0530

    Bash Scripting ~AutoPush

---
+++ 04.variable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# variables in bash scripting
+# This is a very important concept in any programming language or any technology
+# Without this, it quite tough to see the quality of the code or the script
+# variables are used to store some data and use it when ever required
+# a=100
+# In bash, There is no concept of data types
+# In programming languages like C,c++,Java,Python,Etc.. we have different data types like int, float, double, char, string etc
+# But in bash, everything is treated as a string
+
+#Syntax to declare a variable in bash
+# variable_name=value
+#how to print this? Special character $ is used to access the value of the variable
+# echo $variable_name
+
+name="DevSecOps"
+echo "Welcome to $name"
