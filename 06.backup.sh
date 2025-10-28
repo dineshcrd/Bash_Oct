15a7c1635347cb69fab9fe1ad7013689c6c48535
dineshcrd <cloudtechnology129@gmail.com>
2025-10-28 21:36:19 +0530

    Bash Scripting ~AutoPush

---
+++ 06.backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# backup script to backup important files and directories
+# source directories to backup
+SOURCE_DIRS="/home"
+# backup destination directory
+BACKUP_DIR="/tmp/backup"
+# create backup filename with date
+BACKUP_FILE="backup_$(date +%Y%m%d_%H%M%S"
