3a876bd974dadda0311c88320d8c2ff22fef57a1
dineshcrd <cloudtechnology129@gmail.com>
2025-10-30 17:29:22 +0530

    Bash Scripting ~AutoPush

--- Bash/print.sh
+++ Bash/print.sh
@@ -7,5 +7,7 @@ echo "how are you?"
 
 # \n is for new line
 # -e is for enabling interpretation of backslash escapes
-
+# \t is for tab space
 echo -e "hai \n hello \n how are you?"
+
+echo -e "hai \t hello \t how are you?"
