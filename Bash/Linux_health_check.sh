b5d3a4dbfeb30f5dd1bf4d902087d904f29b0a7f
dineshcrd <cloudtechnology129@gmail.com>
2025-10-30 17:42:58 +0530

    Bash Scripting ~AutoPush

--- Bash/Linux_health_check.sh
+++ Bash/Linux_health_check.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 #Linux health check script
+echo "=========================="
 echo "System Health Check Report"
-echo "--------------------------"
+echo "=========================="
+
 #hostname
 echo "Hostname: $(hostname)"
+echo "=========================="
+
 #uptime 
 echo "Uptime: $(uptime -p)"
+echo "=========================="
+
 #disk usage
 echo "Disk Usage:"
 df -h
+echo "=========================="
+
 #memory usage
 echo "Memory Usage:"
 free -h
+
+echo "=========================="
+echo "Health Check Completed."
+echo "=========================="
